aa4a2b1ed981c163556ee21ed75961469ba6b559
EunjoungAhn <aej9300@gmail.com>
2022-11-09 23:39:29 +0900

    통합테스트 교체 후, 마지막까지 작업한 코드 - 에러명: No tests were found

--- src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/example/project/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -9,6 +9,11 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
     @Autowired
     private PharmacyRepository pharmacyRepository
 
+    // 각 메서드 테스트 시작 전에 DB 정리(각 테스트 후, DB에 따른 오류를 방지하기 위해)
+    def setup(){
+        pharmacyRepository.deleteAll()
+    }
+
     def "PharmacyRepository save"() {
         given:
         String address = "서울 특별시 성북구 종암동"
@@ -32,4 +37,29 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         result.getLatitude() == latitude
         result.getLongitude() == longitude
     }
+
+    /*
+    def "PharmacyRepository saveAll"() {
+        given:
+        String address = "서울 특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
+    }
+
+    */
 }
